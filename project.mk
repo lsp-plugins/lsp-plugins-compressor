c6d4e16331ae3ccf507ad543885a54a296ba306f
Vladimir Sadovnikov <sadko4u@gmail.com>
2021-08-17 00:51:57 +0300

    Updated dependencies

--- project.mk
+++ project.mk
@@ -21,7 +21,7 @@
 # Package version
 ARTIFACT_ID                 = LSP_PLUGINS_COMPRESSOR
 ARTIFACT_NAME               = lsp-plugins-compressor
-ARTIFACT_DESC               = Trigger plugin series
+ARTIFACT_DESC               = Compressor plugin series
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_EXPORT_ALL         = 1
 ARTIFACT_VERSION            = 1.0.4-devel
